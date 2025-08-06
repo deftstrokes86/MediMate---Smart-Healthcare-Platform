9d34f00959bc31feda3b19aac91c2217e9d877ba
Stephen Igwebuike <stephenigwebuike975@gmail.com>
2025-08-06 12:44:49 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -49,9 +49,14 @@ const AccordionContent = React.forwardRef<
     className="overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
     {...props}
   >
-    <div className={cn("pb-4 pt-0", className)}>{children}</div>
+    <div
+      className={cn("pb-4 pt-0", className)}
+      {...(props.dangerouslySetInnerHTML ? { dangerouslySetInnerHTML: props.dangerouslySetInnerHTML } : {})}
+    >
+      {children}
+    </div>
   </AccordionPrimitive.Content>
-))
+));
 
 AccordionContent.displayName = AccordionPrimitive.Content.displayName
 
